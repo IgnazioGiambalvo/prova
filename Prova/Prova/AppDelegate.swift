2ea785be6f893bfe63f8e151f63508187bc7fe0c
ignaziogiambalvo <ignazio.giambalvo@gmail.com>
2020-07-03 15:47:22 +0200

    initial project

---
+++ Prova/Prova/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Prova
+//
+//  Created by Ignazio Giambalvo on 03/07/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+    
+}
+
---
+++ Prova/Prova/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prova
+//
+//  Created by Ignazio Giambalvo on 03/07/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
